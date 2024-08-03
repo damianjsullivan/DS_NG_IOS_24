bdb696463bea9bb89b901a5acc7dd2a184871a17
Damian Sullivan <damian@brightstarsoftware.com>
2024-08-03 13:45:21 +0100

    Initial Commit

---
+++ Netgear Books/Netgear_BooksApp.swift
@@ -0,0 +1,32 @@
+//
+//  Netgear_BooksApp.swift
+//  Netgear Books
+//
+//  Created by Damian Sullivan on 03/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Netgear_BooksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
